f16fd79095350413200516025832af51f9ee9bef
shadowslinks <57361311+shadowslinks@users.noreply.github.com>
2019-11-04 16:29:03 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,28 @@
 //Main.c
 #include <stdio.h>
+#include <stdlib.h>
 
+typedef struct caractere {
+  char c;
+  int fond;
+  int couleur;
+}TCaractere; 
+ 
+typedef struct suiteCaractere{
+  TCaractere info;
+  struct suiteCaractere * caractereSuivant;
+}TSuiteCaractere; 
+ 
+typedef struct suiteParagraphe{
+  int numeroParagraphe ;
+  struct suiteParagraphe * paragrapheSuivant;
+  struct suiteParagraphe * paragraphePrecedent;
+  TSuiteCaractere * premierCaractere;
+} TSuiteParagraphe; 
+ 
+typedef TSuiteParagraphe * PParagraphe;
 
+typedef TSuiteCaractere * PCaractere;
 
 void main(void) {
   
